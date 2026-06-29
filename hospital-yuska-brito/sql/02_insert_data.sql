bcb905a155d479fbff446c55bdcdb2c5ad013b30
Gabriel Cesar <gabrielcsoliveirajmj@gmail.com>
2026-06-29 00:17:18 -0300

    correcao dos dados de teste

--- hospital-yuska-brito/sql/02_insert_data.sql
+++ hospital-yuska-brito/sql/02_insert_data.sql
@@ -4,24 +4,21 @@ BEGIN;
 
 -- PESSOAS (5 pacientes + 5 residentes + 5 preceptores = 15)
 INSERT INTO PESSOA (nome, cpf, data_nascimento, is_flamengo, telefone) VALUES
--- pacientes: elenco atual 2025/2026 (id 1-5)
-('Gerson Santos de Oliveira',    '98412376501', '1997-05-20', TRUE, '(21) 98412-3765'),
-('Giorgian De Arrascaeta',       '74523891204', '1994-06-01', TRUE, '(22) 97523-8912'),
-('Nicolas De La Cruz',           '63148927305', '1997-06-01', TRUE, '(11) 99148-9273'),
-('Leonardo Ortiz',               '52736184906', '1996-01-18', TRUE, '(21) 98736-1849'),
-('Erick Pulgar',                 '41925837107', '1994-01-15', TRUE, '(22) 97925-8371'),
--- residentes: melhores do elenco de 2019 (id 6-10)
-('Gabriel Barbosa',              '87614392508', '1996-08-30', TRUE, '(21) 98614-3925'),
-('Everton Ribeiro de Souza',     '76523841209', '1989-01-02', TRUE, '(11) 97652-3841'),
-('Bruno Henrique Pinto',         '65412783910', '1990-10-27', TRUE, '(21) 98541-2783'),
-('Filipe Luis Kasmirski',        '54891632711', '1988-08-09', TRUE, '(22) 97489-1632'),
-('Diego Ribas da Cunha',         '43782591412', '1985-02-28', TRUE, '(21) 98378-2591'),
--- preceptores: campeoes mundiais de 1981 (id 11-15)
-('Arthur Antunes Coimbra',       '32671489513', '1953-03-03', TRUE, '(11) 97267-1489'),
-('Marcos Antonio Batista Junior','21594378614', '1954-06-29', TRUE, '(22) 98159-4378'),
-('Adilio Pereira de Carvalho',   '19483267715', '1955-05-17', TRUE, '(21) 97948-3267'),
-('Paulo Cesar Nunes',            '98372156816', '1956-07-10', TRUE, '(11) 98837-2156'),
-('Leandro Bastos Iorio',         '87261945917', '1954-09-29', TRUE, '(22) 97726-1945');
+('Gerson Santos de Oliveira',    '98412376501', '1997-05-20', TRUE, '(83) 98412-3765'),
+('Giorgian De Arrascaeta',       '74523891204', '1994-06-01', TRUE, '(83) 97523-8912'),
+('Nicolas De La Cruz',           '63148927305', '1997-06-01', TRUE, '(83) 99148-9273'),
+('Leonardo Ortiz',               '52736184906', '1996-01-18', TRUE, '(83) 98736-1849'),
+('Erick Pulgar',                 '41925837107', '1994-01-15', TRUE, '(83) 97925-8371'),
+('Gabriel Barbosa',              '87614392508', '1996-08-30', TRUE, '(83) 98614-3925'),
+('Everton Ribeiro de Souza',     '76523841209', '1989-01-02', TRUE, '(83) 97652-3841'),
+('Bruno Henrique Pinto',         '65412783910', '1990-10-27', TRUE, '(83) 98541-2783'),
+('Filipe Luis Kasmirski',        '54891632711', '1988-08-09', TRUE, '(83) 97489-1632'),
+('Diego Ribas da Cunha',         '43782591412', '1985-02-28', TRUE, '(83) 98378-2591'),
+('Arthur Antunes Coimbra',       '32671489513', '1953-03-03', TRUE, '(83) 97267-1489'),
+('Marcos Antonio Batista Junior','21594378614', '1954-06-29', TRUE, '(83) 98159-4378'),
+('Adilio Pereira de Carvalho',   '19483267715', '1955-05-17', TRUE, '(83) 97948-3267'),
+('Paulo Cesar Nunes',            '98372156816', '1956-07-10', TRUE, '(83) 98837-2156'),
+('Leandro Bastos Iorio',         '87261945917', '1954-09-29', TRUE, '(83) 97726-1945');
 
 -- PACIENTES
 INSERT INTO PACIENTE (id_pessoa, num_convenio, alergias, grupo_sanguineo) VALUES
@@ -44,7 +41,7 @@ INSERT INTO PROFISSIONAL (id_pessoa, crm, data_admissao, especialidade) VALUES
 (14, 'CRM/RJ-200014', '2005-03-14', 'Neurologia'),
 (15, 'CRM/RJ-200015', '2009-07-01', 'Ortopedia');
 
--- RESIDENTES (melhores de 2019)
+-- RESIDENTES
 INSERT INTO RESIDENTE (id_profissional, ano_residencia) VALUES
 (6,  '2022'),
 (7,  '2021'),
@@ -52,7 +49,7 @@ INSERT INTO RESIDENTE (id_profissional, ano_residencia) VALUES
 (9,  '2022'),
 (10, '2023');
 
--- PRECEPTORES (campeoes de 1981)
+-- PRECEPTORES
 INSERT INTO PRECEPTOR (id_profissional, titulacao) VALUES
 (11, 'Doutor'),
 (12, 'Mestre'),
